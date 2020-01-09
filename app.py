5623dfe70fc1b67c718475dce1c50123ef3f6825
tphaterp <tphaterp@gmail.com>
2020-01-09 14:01:59 -0800

    upload file functionality completed

--- app.py
+++ app.py
@@ -1,34 +1,94 @@
-# external libraries
+import base64
+import datetime
+import io
+
 import dash
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_table
 
+import pandas as pd
+
+# Upload feature was taken from: https://dash.plot.ly/dash-core-components/upload
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.config['suppress_callback_exceptions'] = True
-server = app.server
 
-colors = {"white": "#ffffff",
-          "light_grey": "#d2d7df",
-          "box1blue": "#8BBEE8FF ",
-          "box2green": "#A8D5BAFF",
-          "box3blue": "#8BBEE8FF",
-          "box4green": "#A8D5BAFF",
-          "black": "#000000",
-          "light_purple": "#E3D1FB"
-          }
-
-app.layout = html.Div(style={'backgroundColor': colors['white']}, children=[
-    # HEADER
-    html.Div(className="row", style={'backgroundColor': colors['black'], 'border': '1px solid', "padding-left": 5}, children=[
-        html.H3('Pollutants Matter BC – Visualization of Particulate Matter Concentrations',
-                style={'color':colors['white'], 'margin-top':2, 'margin-bottom':2}),
-        html.P('This application tracks weighted monthly averages for pollution data collected from different stations across British Columbia. The measured pollutants, PM2.5 and PM10, refer to atmospheric particulate matter (PM) that have a diameter of less than 2.5 and 10 micrometers, respectively.',
-                style={'color':colors['white'], 'margin-top':2, 'margin-bottom':2})
-    ])
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
 ])
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
